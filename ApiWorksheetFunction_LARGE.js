aa6749052e5c2a91d06ef3dc984d0ca60c7d1349
Eeshaan Sawant <eeshaans1@gmail.com>
2024-06-18 15:49:43 +0530

    changed variable declarations to VAR

--- ApiWorksheetFunction_LARGE.js
+++ ApiWorksheetFunction_LARGE.js
@@ -1,20 +1,20 @@
 builder.CreateFile("xlsx");
 const oWorksheet = Api.GetActiveSheet();
 
-let numbersArr = [4, 13, 27, 56, 46, 79, 22, 12];
+var numbersArr = [4, 13, 27, 56, 46, 79, 22, 12];
 
 // Place the numbers in cells
-for (let i = 0; i < numbersArr.length; i++) {
+for (var i = 0; i < numbersArr.length; i++) {
   oWorksheet.GetRange("A" + (i + 1)).SetValue(numbersArr[i]);
 }
 
-let oFunction = Api.GetWorksheetFunction();
+var oFunction = Api.GetWorksheetFunction();
 
-let oRange = oWorksheet.GetRange("A1:A8");
+var oRange = oWorksheet.GetRange("A1:A8");
 
-let largePostion = 4;
+var largePostion = 4;
 
-let kLargest = oFunction.LARGE(oRange, largePostion);
+var kLargest = oFunction.LARGE(oRange, largePostion);
 
 oWorksheet.GetRange("C1").SetValue(kLargest);
 
