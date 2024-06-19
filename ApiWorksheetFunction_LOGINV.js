e9612a897a672ca97cab9689e5a07dc2bd707fdc
Eeshaan Sawant <eeshaans1@gmail.com>
2024-06-19 11:20:04 +0530

    Added Scripts for LOGINV

---
+++ ApiWorksheetFunction_LOGINV.js
@@ -0,0 +1,24 @@
+builder.CreateFile("xlsx");
+const oWorksheet = Api.GetActiveSheet();
+
+//configure function parameters
+var numbersArr = [0.5, 4, 7];
+
+//set values in cells
+for (var i = 0; i < yValues.length; i++) {
+  oWorksheet.GetRange("A" + (i + 1)).SetValue(numbersArr[i]);
+}
+
+//get parameters
+var probability = oWorksheet.getRange("A1");
+var mean = oWorksheet.getRange("A2");
+var standardDeviation = oWorksheet.GetRange("A3");
+
+//invoke LOGINV method
+var oFunction = Api.GetWorksheetFunction();
+var ans = oFunction.LOGINV(probability, mean, standardDeviation);
+
+//print answer
+oWorksheet.GetRange("C1").SetValue(ans);
+builder.SaveFile("xlsx", "LOGINV.xlsx");
+builder.CloseFile();
