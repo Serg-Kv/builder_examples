f89e1d237c32105c979a885b56ffad22cada6266
Eeshaan Sawant <eeshaans1@gmail.com>
2024-06-25 14:54:58 +0530

    Added scripts for PERCENTILE_EXEC method

---
+++ ApiWorksheetFunction_PERCENTILE_EXEC.js
@@ -0,0 +1,21 @@
+builder.CreateFile("xlsx");
+const oWorksheet = Api.GetActiveSheet();
+
+var valueArr = [1, 3, 5, 7, 0, 2, 4, 6, 7, 5, 3, 5, 10, 8, 6, 4];
+
+// Place the numbers in cells
+for (var i = 0; i < valueArr.length; i++) {
+  oWorksheet.GetRange("A" + (i + 1)).SetValue(valueArr[i]);
+}
+
+//method params
+var inputValArray = oWorksheet.GetRange("A1:A16").GetValue();
+var k = 0.5;
+
+var oFunction = Api.GetWorksheetFunction();
+var ans = oFunction.PERCENTILE_EXEC(inputValArray, k);
+
+oWorksheet.GetRange("C1").SetValue(ans);
+
+builder.SaveFile("xlsx", "PERCENTILE_EXEC.xlsx");
+builder.CloseFile();
