50c83272d24731391dcc74e2551100fa562d6bce
Eeshaan Sawant <eeshaans1@gmail.com>
2024-07-01 14:11:53 +0530

    Added scripts for AND method

---
+++ ApiWorksheetFunction_AND.js
@@ -0,0 +1,20 @@
+builder.CreateFile("xlsx");
+const oWorksheet = Api.GetActiveSheet();
+
+// Place the numbers in cells
+for (var i = 0; i < valueArr.length; i++) {
+  oWorksheet.GetRange("A1").SetValue(12);
+}
+
+// logcial statements
+// statement1 = A1 < 100
+// statement2  = 34 <1 00
+// statement3  = 50 < 100
+
+var oFunction = Api.GetWorksheetFunction();
+var ans = oFunction.AND(A1 < 100, 34 < 100, 50 < 100); //AND logical function
+
+oWorksheet.GetRange("C1").SetValue(ans);
+
+builder.SaveFile("xlsx", "AND.xlsx");
+builder.CloseFile();
